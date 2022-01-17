e8a6128ce0a1064adaba327e60742f7e2e28c453
karol <>
2022-01-17 19:42:55 +0100

    fsf

--- inputs/outputs.tf
+++ inputs/outputs.tf
@@ -1,12 +1,12 @@
-output "last_subs_id" {
-  value = element([for item in var.greencloud : item.id],length(var.greencloud)-1)
-}
+# output "last_subs_id" {
+#   value = element([for item in var.greencloud : item.id],length(var.greencloud)-1)
+# }
 
-output "last_subs_name" {
-  value = element([for item in var.greencloud : item.name],length(var.greencloud)-1)
-}
+# output "last_subs_name" {
+#   value = element([for item in var.greencloud : item.name],length(var.greencloud)-1)
+# }
 
 
-output "last_cidr" {
-  value = element([for item in var.greencloud : item.cidr],length(var.greencloud)-1)
-}
+# output "last_cidr" {
+#   value = element([for item in var.greencloud : item.cidr],length(var.greencloud)-1)
+# }
