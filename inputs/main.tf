84158acd03cadf5dd372dba4f0cd45a43acbde37
karol <>
2022-01-17 21:06:32 +0100

    a lot

--- inputs/main.tf
+++ inputs/main.tf
@@ -1,12 +1,23 @@
 module "tf_workspace" {
 
-  source = "./terraform-tfe-workspace-master"
-
-
+  source = "./tfe-workspace"
+ 
+  terraform_version = "1.0.0"
   organization = "greencloud"
-  name              = each.value.name
-  oauth_token_id =  data.terraform_remote_state.secrets.outputs.oauth_id
-  identifier        = "karolnedza/avx-spoke-azure"
+  name              = "dupa"
 
-  for_each = var.greencloud
+  vcs_repo = {
+      identifier = "karolnedza/avx-spoke-azure",
+      oauth_token_id = "ot-vWFCSYDGESPuNwYU"
+    }
+  variables = {
+    terraform = {
+      cidr = "1.2.3.4",
+      region = "East Europe"
+    }
+    terraform_sensitive = {
+      controller_ip = "1.2.34.4",
+      controller_password =
+    }
+  }
 }
