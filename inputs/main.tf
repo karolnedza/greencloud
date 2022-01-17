490f351e9420b283efe80e145e7a3ad26aa6d8f6
karol <>
2022-01-17 19:30:20 +0100

    init

--- inputs/main.tf
+++ inputs/main.tf
@@ -1,6 +1,7 @@
 module "tf_workspace" {
-  
+  organization = "greencloud"
   for_each = var.greencloud
+
   source = "./terraform-tfe-workspace-master"
   identifier        = "karolnedza/avx-spoke-azure"
   oauth_token_id =  data.terraform_remote_state.secrets.outputs.oauth_id
