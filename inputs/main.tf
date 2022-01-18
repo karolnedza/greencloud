e38c5286a21c8c95eb4c81a62a76b2756754e6fc
karol <>
2022-01-18 11:16:51 +0100

    cleaning

--- inputs/main.tf
+++ inputs/main.tf
@@ -1,22 +1,101 @@
+
 module "tf_workspace" {
 
-  source = "./tfe-workspace"
+  for_each = var.greencloud
+  source  = "app.terraform.io/greencloud/workspace/tfe"
 
   organization = "greencloud"
-  name              = "dupa"
+  
+  name = each.value.name
+  vcs_repo = each.value.vcs_repo
+  variables = each.value.variables
 
-  vcs_repo = {
-      identifier = "karolnedza/avx-spoke-azure",
-      oauth_token_id = "ot-vWFCSYDGESPuNwYU"
-    }
-
-  variables = {
-    terraform = {
-      cidr = "1.2.3.4",
-      region = "East Europe"
-    }
-    terraform_sensitive = {
-      controller_ip = "1.2.34.4"
-    }
-  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# module "tf_workspace_1" {
+
+#  for_each = var.greencloud
+#   source = "./tfe-workspace"
+
+#   organization = "greencloud"
+#   name              = "ws-heinken-1"
+
+#   vcs_repo = {
+#       identifier = "karolnedza/avx-spoke-azure",
+#       oauth_token_id = "ot-vWFCSYDGESPuNwYU"
+#     }
+
+#   variables = {
+#     terraform = {
+#       cidr = "10.10.0.0/24",
+#       region = "Central Europe"
+#     }
+#     terraform_sensitive = {
+#       controller_ip = "1.2.34.4"
+#     }
+#   }
+# }
+
+
+
+# module "tf_workspace_2" {
+
+#  for_each = var.greencloud
+#   source = "./tfe-workspace"
+
+#   organization = "greencloud"
+#   name              = "ws-heinken-2"
+
+#   vcs_repo = {
+#       identifier = "karolnedza/avx-spoke-azure",
+#       oauth_token_id = "ot-vWFCSYDGESPuNwYU"
+#     }
+
+#   variables = {
+#     terraform = {
+#       cidr = "10.22.0.0/24",
+#       region = "West Europe"
+#     }
+#     terraform_sensitive = {
+#       controller_ip = "1.2.34.4"
+#     }
+#   }
+# }
--- inputs/variables.tf
+++ inputs/variables.tf
@@ -1,21 +1,38 @@
-#
-# variable greencloud {
-#   type     = map
-#   default  = {
-#     sub1 = {
-#       name  = "hnk-1-new"
-#       id = "12112-4fdsf-32",
-#       terraform = {
-#         cidr = "1.2.3.4",
-#         region = "East Europe"
-#       }
-#     },
-#     sub2 = {
-#       name    = "hnk-2-new",
-#       id = "fffff-ff-32",
-#       terraform = {
-#         cidr = "10.20.0.2",
-#         region_name = "West Europe"}
-#     }
-#   }
-# }
+
+variable greencloud {
+  type     = map
+  default  = {
+        sub1 = {
+                name = "heineken-1"
+                vcs_repo = {
+                    identifier = "karolnedza/avx-spoke-azure",
+                    oauth_token_id = "ot-vWFCSYDGESPuNwYU"
+             }
+                variables = {
+                    terraform = {
+                    cidr = "10.10.0.0/24",
+                    region = "Central Europe"
+                 }
+                terraform_sensitive = {
+                    controller_ip = "1.2.34.4"
+                 }
+            }
+        },
+        sub2 = {
+                name = "heineken-2"
+                vcs_repo = {
+                    identifier = "karolnedza/avx-spoke-azure",
+                    oauth_token_id = "ot-vWFCSYDGESPuNwYU"
+             }
+                variables = {
+                    terraform = {
+                    cidr = "10.20.0.0/24",
+                    region = "West Europe"
+                 }
+                terraform_sensitive = {
+                    controller_ip = "11.2.34.4"
+                 }
+            }
+        }
+    }
+}
