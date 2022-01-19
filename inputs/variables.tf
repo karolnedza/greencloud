751dd3ade35e0977e36e69c0085875d358367a85
karol <>
2022-01-19 13:26:12 +0100

    cleaning

--- inputs/variables.tf
+++ inputs/variables.tf
@@ -2,27 +2,28 @@
 variable greencloud {
   type     = map
   default  = {
-        sub1 = {
-                name = "heineken-1"  # Workspace name 
-                variables = {
-                    terraform = {
-                    arm_subscription_id = "4a22a878-65d2-4ff7-ada3-a29e475bd1ea"
-                    cidr = "10.110.0.0/24",
-                    region = "Central US"
-                    name = "green-cloud-1"   # Name for Vnet/GW/Account
-                 }
-            }
-        },
-         sub2 = {
-                name = "heineken-2"
-                variables = {
-                    terraform = {
-                    arm_subscription_id = "4a22a878-65d2-4ff7-ada3-a29e475bd1ea"
-                    cidr = "10.112.0.0/24",
-                    region = "Germany West Central"
-                    name = "greennow"
-                 }
-            }
-        }
+        # sub1 = {
+        #         name = "heineken-1"  # Workspace name 
+        #         variables = {
+        #             terraform = {
+        #             arm_subscription_id = "4a22a878-65d2-4ff7-ada3-a29e475bd1ea"
+
+        #             cidr = "10.110.0.0/24",
+        #             region = "Central US"
+        #             name = "green-cloud-1"   # Name for Vnet/GW/Account
+        #          }
+        #     }
+        # },
+        #  sub2 = {
+        #         name = "heineken-2"
+        #         variables = {
+        #             terraform = {
+        #             arm_subscription_id = "4a22a878-65d2-4ff7-ada3-a29e475bd1ea"
+        #             cidr = "10.112.0.0/24",
+        #             region = "Germany West Central"
+        #             name = "greennow"
+        #          }
+        #     }
+        # }
     }
 }
