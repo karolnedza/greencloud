3ebb3f9e5aa099802f2171e38dd0390420a24c6f
karol <>
2022-08-31 13:33:00 +0200

    new customer

--- inputs/variables.tf
+++ inputs/variables.tf
@@ -6,10 +6,9 @@ variable greencloud {
                 name = "heineken-1"  # Workspace name
                 variables = {
                     terraform = {
-                    arm_subscription_id = "4a22a878-65d2-4ff7-ada3-a29e475bd1ea"
-
+                    arm_subscription_id = "bd793cff-d6fc-4eb4-a71c-24f06dc8dff7"
                     cidr = "10.110.0.0/24",
-                    region = "Central US"
+                    region = "West Europe"
                     name = "green-cloud-1"   # Name for Vnet/GW/Account
                  }
             }
