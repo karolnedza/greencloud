fe3b8fb07544a77ffe34d63cbed36d0416d4a4c1
karol <>
2022-08-31 18:45:16 +0200

    adding heineken

--- inputs/variables.tf
+++ inputs/variables.tf
@@ -2,23 +2,23 @@
 variable greencloud {
   type     = map
   default  = {
-        # sub1 = {
-        #         name = "heineken-1"  # Workspace name
-        #         variables = {
-        #             terraform = {
-        #             arm_subscription_id = "bd793cff-d6fc-4eb4-a71c-24f06dc8dff7"
-        #             cloud = "azure",
-        #             cidr = "10.110.0.0/24",
-        #             region = "West Europe",
-        #             name = "green-cloud-1"   # Name for Vnet/GW/Account
-        #          }
-        #     }
-        # },
+        sub1 = {
+                name = "heineken-1"  # Workspace name
+                variables = {
+                    terraform = {
+                    arm_subscription_id = "bd793cff-d6fc-4eb4-a71c-24f06dc8dff7"
+                    cloud = "azure",
+                    cidr = "10.110.0.0/24",
+                    region = "West Europe",
+                    name = "az-heineken-spoke-1"   # Name for Vnet/GW/Account
+                 }
+            }
+        },
         #  sub2 = {
         #         name = "heineken-2"
         #         variables = {
         #             terraform = {
-        #             arm_subscription_id = "4a22a878-65d2-4ff7-ada3-a29e475bd1ea"
+        #             arm_subscription_id = "bd793cff-d6fc-4eb4-a71c-24f06dc8dff7"
         #             cidr = "10.112.0.0/24",
         #             region = "Germany West Central"
         #             name = "greennow"
